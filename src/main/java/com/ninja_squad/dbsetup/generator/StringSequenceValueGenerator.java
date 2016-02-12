b64cb44a76ac016c49b9b084e2e27ddef056c713
jbnizet <jb@ninja-squad.com>
2016-02-12 20:06:42 +0100

    Update copyright dates

--- src/main/java/com/ninja_squad/dbsetup/generator/StringSequenceValueGenerator.java
+++ src/main/java/com/ninja_squad/dbsetup/generator/StringSequenceValueGenerator.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2013, Ninja Squad
+ * Copyright (c) 2013-2016, Ninja Squad
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
