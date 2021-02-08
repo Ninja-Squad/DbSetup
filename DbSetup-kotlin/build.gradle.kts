578a23bf72d9523f3134af0624760bd71c9c635f
jnizet <jb@ninja-squad.com>
2021-02-08 23:07:15 +0100

    build: add dokka

--- DbSetup-kotlin/build.gradle.kts
+++ DbSetup-kotlin/build.gradle.kts
@@ -3,6 +3,12 @@ import java.time.Duration
 plugins {
     `java-library-convention`
     kotlin("jvm") version "1.4.20"
+    id("org.jetbrains.dokka") version "1.4.20"
+}
+
+repositories {
+    // TODO replace this by other repo once dokka allows it
+    jcenter()
 }
 
 project.description = "Kotlin extensions for DbSetup"
@@ -11,4 +17,25 @@ dependencies {
     api(project(":DbSetup"))
     testImplementation("junit:junit:4.+")
     testImplementation("org.mockito:mockito-all:1.9.0")
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveFileName.set("dokka-html.jar")
+    from(tasks.dokkaJavadoc)
+}
+
+val dokkaJar by tasks.registering(Jar::class) {
+    archiveFileName.set("dokka-javadoc.jar")
+    from(tasks.dokkaHtml)
+}
+
+publishing {
+    publications.named<MavenPublication>("maven") {
+        artifact(dokkaJar) {
+            classifier = "dokka"
+        }
+        artifact(javadocJar) {
+            classifier = "javadoc"
+        }
+    }
 }
--- buildSrc/src/main/kotlin/java-library-convention.gradle.kts
+++ buildSrc/src/main/kotlin/java-library-convention.gradle.kts
@@ -15,7 +15,6 @@ version = rootProject.version
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
-    withJavadocJar()
     withSourcesJar()
 }
 
