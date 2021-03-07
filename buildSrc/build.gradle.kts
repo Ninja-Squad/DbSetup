8aa42d1db972a8e59fc8f3f9ee2d8d0dbbb29a60
jnizet <jb@ninja-squad.com>
2021-03-07 19:37:38 +0100

    build: use publish-plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,3 @@ repositories {
     gradlePluginPortal()
 }
 
-dependencies {
-    implementation("de.marcphilipp.gradle:nexus-publish-plugin:0.4.0")
-    implementation("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:0.22.0")
-}
